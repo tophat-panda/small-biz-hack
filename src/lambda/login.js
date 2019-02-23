f4196a007f45e92efcae6ca4145724b2c2670f33
Stefan Trinh <stefantrinh1.work@gmail.com>
2019-02-23 17:20:18 +0000

    merge

---
+++ src/lambda/login.js
@@ -0,0 +1,48 @@
+import response from "./util/response";
+import getDatabaseConnection from "./util/database";
+import passwordHelpers from "./util/password-helpers";
+import jwt from "jsonwebtoken";
+
+export async function handler(event, context) {
+  if (event.httpMethod !== "POST") {
+    return response({
+      statusCode: 405,
+      body: { message: "you must make a Post Request" }
+    });
+  }
+  let username;
+  let password;
+  try {
+    const body = JSON.parse(event.body);
+    username = body.username;
+    password = body.password;
+  } catch (error) {
+    console.log(error);
+    if (error instanceof SyntaxError) {
+      return response({
+        statusCode: 400,
+        body: { message: "Bad request" }
+      });
+    }
+    return response({
+      statusCode: 500,
+      body: { message: "Unknown error" }
+    });
+  }
+  const { dbo, close } = await getDatabaseConnection();
+
+  const users = dbo.collection("users");
+  const findUser = await users.find({ username });
+  const match = await passwordHelpers.compare(password, findUser.hash);
+
+  if (!findUser || !match) {
+    await close();
+    return response({
+      statusCode: 401,
+      body: { message: "Bad request" }
+    });
+  }
+  await close();
+
+  return response({ body: username }); //   return response({ body: { token: jwt.sign(username, process.env.SECRET) } });
+}
