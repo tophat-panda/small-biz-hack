1a4203cb292528dce221f8d954d9b286376623ed
Stefan Trinh <stefantrinh1.work@gmail.com>
2019-02-23 14:05:50 +0000

    added password hasher

--- src/lambda/register.js
+++ src/lambda/register.js
@@ -1,5 +1,40 @@
-import response from "./util/response"
+import response from "./util/response";
+import getDatabaseConnection from "./util/database";
+import bcrypt from "bcrypt";
 
-export async function handler(event, context ) {
-    return response({body:7})
+export async function handler(event, context) {
+  if (event.httpMethod !== "POST") {
+    return response({
+      statusCode: 405,
+      body: { message: "you must make a Post Request" }
+    });
+  }
+  let username;
+  let password;
+  try {
+    const body = JSON.parse(body);
+    username = body.username;
+    password = body.password;
+  } catch (e) {
+    console.log(e);
+    if (e instanceof SyntaxError) {
+      return response({
+        statusCode: 400,
+        body: { message: "Bad request" }
+      });
+    }
+    return response({
+      statusCode: 500,
+      body: { message: "Unknown error" }
+    });
+  }
+  //   const { dbo, close } = await getDatabaseConnection();
+
+  //   const users = dbo.collection("users");
+
+  const hash = bcrypt.hash(password, 10);
+
+  //   await close();
+
+  return response({ body: { username, hash } });
 }
