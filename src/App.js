b6dc7d87ed21c6f89b48227eaf4bf4faa7692edd
Luke Harris <lukeharris01170@gmail.com>
2019-02-23 22:07:56 +0000

    WIP: accept calls

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 
-import Header from './components/header';
+import Header from "./components/header";
 
-import homepg from './pages/home';
-import registerpg from './pages/register';
-import loginpg from './pages/login';
+import homepg from "./pages/home";
+import registerpg from "./pages/register";
+import loginpg from "./pages/login";
 
-import Footer from './components/footer';
+import Footer from "./components/footer";
 
-import './App.scss';
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.scss";
 
 library.add(faUser);
 library.add(faLock);
@@ -20,18 +23,20 @@ library.add(faLock);
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header title="TopHat Panda" />
-          <Switch>
-            <Route exact path="/" component={homepg} />
-            <Route exact path="/login" component={loginpg} />
-            <Route exact path="/register" component={registerpg} />
-            {/* <Route path='*' component={Not_found_pg} /> */}
-          </Switch>
-          <Footer date={new Date().getFullYear()} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header title="TopHat Panda" />
+            <Switch>
+              <Route exact path="/" component={homepg} />
+              <Route exact path="/login" component={loginpg} />
+              <Route exact path="/register" component={registerpg} />
+              {/* <Route path='*' component={Not_found_pg} /> */}
+            </Switch>
+            <Footer date={new Date().getFullYear()} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
