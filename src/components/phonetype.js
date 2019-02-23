356f5c8d5fc48e7d71e02af361dd1cdadb044bc8
Stefan Trinh <stefantrinh1.work@gmail.com>
2019-02-23 23:22:22 +0000

    add sidebar

---
+++ src/components/phonetype.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Phonetype extends Component {
+  render() {
+    return (
+      <div className="phone-type">
+        <p>Phone or Mobile</p>
+      </div>
+
+    );
+  }
+}
+
+export default Phonetype;
