155e78dbffabac63717336e8e48567c00b39727a
Luke Harris <lukeharris01170@gmail.com>
2019-02-23 23:20:02 +0000

    Create call state machine

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,5 +1,29 @@
 import { combineReducers } from "redux";
 
 export default combineReducers({
-  foo: () => "bar"
+  incoming_call: (state = false, action) => {
+    switch (action.type) {
+      case "INCOMING_CALL":
+        console.log(action.payload);
+        return true;
+      case "SET_CALL_STATUS":
+        if (action.payload !== "started") {
+          return false;
+        }
+        return state;
+      default:
+        return state;
+    }
+  },
+  call_in_progress: (state = false, action) => {
+    switch (action.type) {
+      case "SET_CALL_STATUS":
+        if (action.payload === "answered") {
+          return true;
+        }
+        return false;
+      default:
+        return state;
+    }
+  }
 });
--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
 window.answer = null;
+window.reject = null;
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
@@ -12,6 +13,8 @@ const store = createStore(
       switch (action.type) {
         case "ANSWER_CALL":
           window.answer && window.answer();
+        case "REJECT_CALL":
+          window.reject && window.reject();
       }
       next(action);
     })
@@ -23,14 +26,16 @@ console.log(process.env.REACT_APP_NEXMO_TOKEN);
 new window.ConversationClient()
   .login(process.env.REACT_APP_NEXMO_TOKEN)
   .then(application => {
-    console.log("initialized");
     window.nexmo = application;
 
     application.on("member:call", (member, call) => {
-      console.log("INCOMING CALL");
+      store.dispatch({ type: "INCOMING_CALL", payload: call });
       window.answer = () => call.answer();
+      window.reject = () => call.reject();
     });
-    application.on("call:state:change");
+    application.on("call:status:changed", call => {
+      store.dispatch({ type: "SET_CALL_STATUS", payload: call.status });
+    });
   });
 
 export default store;
