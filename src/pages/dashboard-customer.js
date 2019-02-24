00dc3e507eaea7ec0fce61111d318cf1e27aac74
Luke Harris <lukeharris01170@gmail.com>
2019-02-24 09:18:25 +0000

    Add customer card to customer dashboard

--- src/pages/dashboard-customer.js
+++ src/pages/dashboard-customer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Table from "../components/table";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const data = {
   _id: "5c720df9158ab7231880842c",
@@ -12,32 +13,70 @@ const data = {
       id: "5c720e2d158ab7231880842d",
       name: "Water Bottle",
       price: 6.99
+    },
+    {
+      id: "5c720e2d158ab7231880842d",
+      name: "Rucksack",
+      price: 9.99
     }
   ]
 };
 
+const products = [
+  {
+    id: "5c720e2d158ab7231880842d",
+    name: "Water Bottle",
+    price: 6.99
+  },
+  {
+    id: "5c720e2d158ab7231880842d",
+    name: "Budget trainers",
+    price: 4.99
+  },
+  {
+    id: "5c720e2d158ab7231880842d",
+    name: "Tracksuit",
+    price: 11.59
+  },
+  {
+    id: "5c720e2d158ab7231880842d",
+    name: "Rucksack",
+    price: 9.99
+  }
+];
+
 class DashboardCustomer extends Component {
+  state = {
+    extraOrders: []
+  };
   render() {
     return (
       <div>
-        <h2>
-          {data.first_name} {data.last_name}
-        </h2>
-        <p>t:{data.phone_number}</p>
-        <p>e:{data.email}</p>
-         <Table
+        <div className="customerHeader">
+          <div className="customerCard">
+            <h2>
+              {data.first_name} {data.last_name}
+            </h2>
+            <p>
+              <FontAwesomeIcon icon="phone" /> {data.phone_number}
+            </p>
+            <p>
+              <FontAwesomeIcon icon="envelope" /> {data.email}
+            </p>
+          </div>
+          <div className="orderBlock">
+            <h3>Recent orders</h3>
+            <Table
+              noView
               link={id => `/dashboard/customer/${id}`}
               headers={[
                 { key: "name", text: "Product Name" },
-                { key: "price", text: "Price" },
-
+                { key: "price", text: "Price" }
               ]}
               data={data.orders}
             />
-
-
-
-
+          </div>
+        </div>
       </div>
     );
   }
